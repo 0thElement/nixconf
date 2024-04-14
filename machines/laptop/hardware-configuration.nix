3c3b99a9d57d51ad0b69ed43aa681fc8ac915768
0thElement <quangquangtrung89@gmail.com>
2024-04-14 21:43:24 +0700

    setup swap

--- machines/laptop/hardware-configuration.nix
+++ machines/laptop/hardware-configuration.nix
@@ -29,7 +29,10 @@
       options = [ "nofail" "rw" "uid=1000"];
     };
 
-  swapDevices = [ ];
+    swapDevices = [ {
+      device = "/var/lib/swapfile";
+      size = 8 * 1024;
+    } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
